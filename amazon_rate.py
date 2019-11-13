3eb64e1e0e98175372f4353a0e8aaced93ad27af
manojitballav <manojitocto@gmail.com>
2019-11-13 20:10:02 +0530

    Error handling added

--- amazon_rate.py
+++ amazon_rate.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 from selenium import webdriver
 import re,time
+from selenium.common.exceptions import NoSuchElementException
 
 # database connections
 client = MongoClient('10.56.133.14',27017)
-db = client['Manish']
+db = client['Projector']
 col = db['amazon']
 
 def five_star_scrap(pc,fiver):
@@ -147,63 +148,78 @@ def number(pc):
     print(str(pc)+': Reviews: '+str(review)+' Ratings: '+str(rating))
 
     # getting number of one_star reviews
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=one_star&pageNumber=1&sortBy=recent')
-    # driver.implicitly_wait(3)
-    oner = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
-    oner = oner.replace('-','')
-    tmp1 = re.findall(r'\d+',oner)
-    res1 = list(map(int,tmp1))
-    oner = int(res1[1])
+    try:
+        driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=one_star&pageNumber=1&sortBy=recent')
+        # driver.implicitly_wait(3)
+        oner = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
+        oner = oner.replace('-','')
+        tmp1 = re.findall(r'\d+',oner)
+        res1 = list(map(int,tmp1))
+        oner = int(res1[1])
+        one_star_scrap(pc,oner)
+    except NoSuchElementException:
+        pass
 
     # getting the number of two_star reviews
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=two_star&pageNumber=1&sortBy=recent')
-    twor = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
-    twor = twor.replace('-','')
-    tmp2 = re.findall(r'\d+',twor)
-    res2 = list(map(int,tmp2))
-    twor = int(res2[1])
+    try:
+        driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=two_star&pageNumber=1&sortBy=recent')
+        twor = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
+        twor = twor.replace('-','')
+        tmp2 = re.findall(r'\d+',twor)
+        res2 = list(map(int,tmp2))
+        twor = int(res2[1])
+        two_star_scrap(pc,twor)
+    except NoSuchElementException:
+        pass
 
     # getting the number of three_star reviews
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=three_star&pageNumber=1&sortBy=recent')
-    threer = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
-    threer = threer.replace('-','')
-    tmp3 = re.findall(r'\d+',threer)
-    res3 = list(map(int,tmp3))
-    threer = int(res3[1])
+    try:
+        driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=three_star&pageNumber=1&sortBy=recent')
+        threer = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
+        threer = threer.replace('-','')
+        tmp3 = re.findall(r'\d+',threer)
+        res3 = list(map(int,tmp3))
+        threer = int(res3[1])
+        three_star_scrap(pc,threer)
+    except NoSuchElementException:
+        pass
 
     # getting the number of four_star reviews
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=four_star&pageNumber=1&sortBy=recent')
-    fourr = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
-    fourr = fourr.replace('-','')
-    tmp4 = re.findall(r'\d+',fourr)
-    res4 = list(map(int,tmp4))
-    fourr = int(res4[1])
+    try:
+        driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=four_star&pageNumber=1&sortBy=recent')
+        fourr = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
+        fourr = fourr.replace('-','')
+        tmp4 = re.findall(r'\d+',fourr)
+        res4 = list(map(int,tmp4))
+        fourr = int(res4[1])
+        four_star_scrap(pc,fourr)
+    except NoSuchElementException:
+        pass
 
     # getting the number of five_star reviews
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=five_star&pageNumber=1&sortBy=recent')
-    fiver = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
-    fiver = fiver.replace('-','')
-    tmp5 = re.findall(r'\d+',fiver)
-    res5 = list(map(int,tmp5))
-    fiver = int(res5[1])
+    try:
+        driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_sr?ie=UTF8&amp%3BshowViewpoints=1&filterByStar=five_star&pageNumber=1&sortBy=recent')
+        fiver = driver.find_element_by_xpath('//*[@id="filter-info-section"]/span[1]').text
+        fiver = fiver.replace('-','')
+        tmp5 = re.findall(r'\d+',fiver)
+        res5 = list(map(int,tmp5))
+        fiver = int(res5[1])
+        five_star_scrap(pc,fiver)
+    except NoSuchElementException:
+        pass
 
     driver.quit()
 
     # printing the breakup of reviews along ratings
-    print('1 Star: '+str(oner))
-    print('2 Star: '+str(twor))
-    print('3 Star: '+str(threer))
-    print('4 Star: '+str(fourr))
-    print('5 Star: '+str(fiver))
-    one_star_scrap(pc,oner)
-    two_star_scrap(pc,twor)
-    three_star_scrap(pc,threer)
-    four_star_scrap(pc,fourr)
-    five_star_scrap(pc,fiver)
+    # print('1 Star: '+str(oner))
+    # print('2 Star: '+str(twor))
+    # print('3 Star: '+str(threer))
+    # print('4 Star: '+str(fourr))
+    # print('5 Star: '+str(fiver))
 
 def read():
-    for doc in col.find({'pc':{"$in":["B07DJ8K2KT"]}}):
-    # for doc in col.find({}):
+    # for doc in col.find({'pc':{"$in":["B07DJ8K2KT"]}}):
+    for doc in col.find({}):
         number(doc['pc'])
 
 if __name__ == '__main__':
