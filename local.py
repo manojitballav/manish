76ba0d6e8e9d41ae1742c80d6a82bf61f1d0dc4d
manojitballav <manojitocto@gmail.com>
2021-05-24 18:24:12 +0530

    changed priorities

--- local.py
+++ local.py
@@ -7,10 +7,11 @@ from selenium.common.exceptions import NoSuchElementException
 
 # database connections
 client = MongoClient('10.56.146.102',27017)
-# db = client['Manish']
-db = client['manish']
+db = client['Manish']
+# db = client['manish']
 col = db['asin']
-pc = 'B08RSY2653'
+pc = 'B08T2NBQRH'
+# pc = 'B08RSY2653'
 
 #reading the number of reviews from Amazon Product Page
 driver = webdriver.Chrome()
@@ -70,168 +71,168 @@ for val in range(1, number+1):
         print(count)
 
 # 2 star reviews
-driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=two_star&pageNumber=1&sortBy=recent')
-number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
-print(number)
-number = number.replace(' global ratings','')
-number  = number.replace(' global reviews','')
-number  = number.replace('|','')
-number = number.replace(',','')
-number = list(map(int, number.split()))
-number = (number[1])
-number = int(number)
-print("Number of two star reviews are: "+str(number))
-#scraping the reviews
-#method to scrap the reviews
-collection = db[pc]
-rating = 2
-count = 0
-for val in range(1, number+1):
-    val = str(val)
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=two_star&pageNumber='+val+'&sortBy=recent')    
-    for mal in range(1,11):
-        try:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
-        except Exception as e:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
-            print(e)
-        try:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
-        except Exception as e:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
-            print(e)
-        date = date.replace('Reviewed in India on ','')
-        try:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
-        except Exception as e:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
-            print(e)
-        collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
-        count = count+1
-        print(count)
+# driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=two_star&pageNumber=1&sortBy=recent')
+# number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
+# print(number)
+# number = number.replace(' global ratings','')
+# number  = number.replace(' global reviews','')
+# number  = number.replace('|','')
+# number = number.replace(',','')
+# number = list(map(int, number.split()))
+# number = (number[1])
+# number = int(number)
+# print("Number of two star reviews are: "+str(number))
+# #scraping the reviews
+# #method to scrap the reviews
+# collection = db[pc]
+# rating = 2
+# count = 0
+# for val in range(1, number+1):
+#     val = str(val)
+#     driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=two_star&pageNumber='+val+'&sortBy=recent')    
+#     for mal in range(1,11):
+#         try:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
+#         except Exception as e:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
+#             print(e)
+#         try:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
+#         except Exception as e:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
+#             print(e)
+#         date = date.replace('Reviewed in India on ','')
+#         try:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
+#         except Exception as e:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
+#             print(e)
+#         collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
+#         count = count+1
+#         print(count)
 
 # 3 star reviews
-driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=three_star&pageNumber=1&sortBy=recent')
-number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
-print(number)
-number = number.replace(' global ratings','')
-number  = number.replace(' global reviews','')
-number  = number.replace('|','')
-number = number.replace(',','')
-number = list(map(int, number.split()))
-number = (number[1])
-number = int(number)
-print("Number of three star reviews are: "+str(number))
-# scraping the reviews
-# method to scrap the reviews
-collection = db[pc]
-rating = 3
-count = 0
-for val in range(1, number+1):
-    val = str(val)
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=three_star&pageNumber='+val+'&sortBy=recent')    
-    for mal in range(1,11):
-        try:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
-        except Exception as e:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
-            print(e)
-        try:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
-        except Exception as e:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
-            print(e)
-        date = date.replace('Reviewed in India on ','')
-        try:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
-        except Exception as e:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
-            print(e)
-        collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
-        count = count+1
-        print(count)
+# driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=three_star&pageNumber=1&sortBy=recent')
+# number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
+# print(number)
+# number = number.replace(' global ratings','')
+# number  = number.replace(' global reviews','')
+# number  = number.replace('|','')
+# number = number.replace(',','')
+# number = list(map(int, number.split()))
+# number = (number[1])
+# number = int(number)
+# print("Number of three star reviews are: "+str(number))
+# # scraping the reviews
+# # method to scrap the reviews
+# collection = db[pc]
+# rating = 3
+# count = 0
+# for val in range(1, number+1):
+#     val = str(val)
+#     driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=three_star&pageNumber='+val+'&sortBy=recent')    
+#     for mal in range(1,11):
+#         try:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
+#         except Exception as e:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
+#             print(e)
+#         try:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
+#         except Exception as e:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
+#             print(e)
+#         date = date.replace('Reviewed in India on ','')
+#         try:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
+#         except Exception as e:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
+#             print(e)
+#         collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
+#         count = count+1
+#         print(count)
 
 
 # 4 star reviews
-driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=four_star&pageNumber=1&sortBy=recent')
-number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
-print(number)
-number = number.replace(' global ratings','')
-number  = number.replace(' global reviews','')
-number  = number.replace('|','')
-number = number.replace(',','')
-number = list(map(int, number.split()))
-number = (number[1])
-number = int(number)
-print("Number of four star reviews are: "+str(number))
-#scraping the reviews
-#method to scrap the reviews
-collection = db[pc]
-rating = 4
-count = 0
-for val in range(1, number+1):
-    val = str(val)
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=four_star&pageNumber='+val+'&sortBy=recent')    
-    for mal in range(1,11):
-        try:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
-        except Exception as e:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
-            print(e)
-        try:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
-        except Exception as e:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
-            print(e)
-        date = date.replace('Reviewed in India on ','')
-        try:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
-        except Exception as e:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
-            print(e)
-        collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
-        count = count+1
-        print(count)
+# driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=four_star&pageNumber=1&sortBy=recent')
+# number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
+# print(number)
+# number = number.replace(' global ratings','')
+# number  = number.replace(' global reviews','')
+# number  = number.replace('|','')
+# number = number.replace(',','')
+# number = list(map(int, number.split()))
+# number = (number[1])
+# number = int(number)
+# print("Number of four star reviews are: "+str(number))
+# #scraping the reviews
+# #method to scrap the reviews
+# collection = db[pc]
+# rating = 4
+# count = 0
+# for val in range(1, number+1):
+#     val = str(val)
+#     driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=four_star&pageNumber='+val+'&sortBy=recent')    
+#     for mal in range(1,11):
+#         try:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
+#         except Exception as e:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
+#             print(e)
+#         try:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
+#         except Exception as e:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
+#             print(e)
+#         date = date.replace('Reviewed in India on ','')
+#         try:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
+#         except Exception as e:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
+#             print(e)
+#         collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
+#         count = count+1
+#         print(count)
 
 
 # 5 star reviews
-driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=five_star&pageNumber=1&sortBy=recent')
-number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
-print(number)
-number = number.replace(' global ratings','')
-number  = number.replace(' global reviews','')
-number  = number.replace('|','')
-number = number.replace(',','')
-number = list(map(int, number.split()))
-number = (number[1])
-number = int(number)
-print("Number of five star reviews are: "+str(number))
-#scraping the reviews
-#method to scrap the reviews
-collection = db[pc]
-rating = 5
-count = 0
-for val in range(1, number+1):
-    val = str(val)
-    driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=five_star&pageNumber='+val+'&sortBy=recent')    
-    for mal in range(1,11):
-        try:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
-        except Exception as e:
-            heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
-            print(e)
-        try:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
-        except Exception as e:
-            date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
-            print(e)
-        date = date.replace('Reviewed in India on ','')
-        try:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
-        except Exception as e:
-            review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
-            print(e)
-        collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
-        count = count+1
-        print(count)
-driver.quit()
+# driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_arp_d_viewopt_srt?filterByStar=five_star&pageNumber=1&sortBy=recent')
+# number = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[4]/div[2]/span').text
+# print(number)
+# number = number.replace(' global ratings','')
+# number  = number.replace(' global reviews','')
+# number  = number.replace('|','')
+# number = number.replace(',','')
+# number = list(map(int, number.split()))
+# number = (number[1])
+# number = int(number)
+# print("Number of five star reviews are: "+str(number))
+# #scraping the reviews
+# #method to scrap the reviews
+# collection = db[pc]
+# rating = 5
+# count = 0
+# for val in range(1, number+1):
+#     val = str(val)
+#     driver.get('https://www.amazon.in/product-reviews/'+pc+'/ref=cm_cr_getr_d_paging_btm_next_'+val+'?ie=UTF8&reviewerType=all_reviews&filterByStar=five_star&pageNumber='+val+'&sortBy=recent')    
+#     for mal in range(1,11):
+#         try:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[2]/a[2]/span')
+#         except Exception as e:
+#             heading = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[2]/a[2]/span[1]')
+#             print(e)
+#         try:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/span').text
+#         except Exception as e:
+#             date = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/span').text
+#             print(e)
+#         date = date.replace('Reviewed in India on ','')
+#         try:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal)+']/div/div/div[4]/span/span')
+#         except Exception as e:
+#             review = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div[1]/div/div[1]/div[5]/div[3]/div/div['+str(mal+1)+']/div/div/div[4]/span/span[1]')
+#             print(e)
+#         collection.insert_one({"review":review.text,"date":date,"heading":heading.text,"rating":rating})
+#         count = count+1
+#         print(count)
+# driver.quit()
